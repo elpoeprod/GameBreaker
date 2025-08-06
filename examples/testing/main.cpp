91aac459a5893abc61fba07495142aac4ae94894
elpoeprod <elpoepstudios@gmail.com>
2025-08-06 22:22:03 +0300

    fixxes

--- examples/testing/main.cpp
+++ examples/testing/main.cpp
@@ -1,5 +1,5 @@
 #define GB_USE_SMALL_FUNCNAMES
-#include "../../include/gamebreaker.hpp"
+#include <gamebreaker/gamebreaker.hpp>
 namespace gb=GameBreaker;
 
 GBObject *obj1;
@@ -22,7 +22,7 @@ void my_draw2() {
 int main() {
     gb::init(GB_WINPOS_CENTER,GB_WINPOS_CENTER,640,480,"Test");
 
-    sprite0=sprite::add("elpaudio.png",1,0,0);
+    sprite0=sprite::add_ext("examples/testing/elpaudio.png",1,0,0,5,5,0,0);
     obj1=object::add(sprite0,nullptr);
     obj2=object::add(sprite0,nullptr);
     obj1->event_draw=my_draw1;
@@ -32,7 +32,7 @@ int main() {
     var room0=room::add(640,480);
     room::add_instance(room0,obj1, 0,0,nullptr);
     room::add_instance(room0,obj2, 10,10,nullptr);
-
+	room::camera_setup(room0,0,1,(GB_CamSetup){0,0,640,480,0},(GB_CamSetup){0,0,640,480,0},-1,(GB_CamTarget){0,0,0,0});
     room::current(room0);
 
     gb::run();
