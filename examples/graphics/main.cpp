12a9ae0714a98ca575415c561980e1bb21cca9b3
elpoeprod <elpoepstudios@gmail.com>
2025-08-15 11:45:43 +0300

    scary update

--- examples/graphics/main.cpp
+++ examples/graphics/main.cpp
@@ -11,12 +11,15 @@ void mydraw() {
 }
 
 int main() {
-    gb::init(GB_WINPOS_CENTER,GB_WINPOS_CENTER,640,480,"Graphics test");
+    gb::init(GB_WINPOS_CENTER,GB_WINPOS_CENTER,"Graphics test");
 
     sprLinus=sprite::add("creator of school.jpg",0,0,0);
     objLinus=object::add(sprLinus,nullptr);
     objLinus->event_draw=mydraw;
-
+    
+    var mysong=audio::add("endless.ogg",gb::GB_MUSIC);
+    audio::loop(mysong,-1);
+    
     room1=room::add(640,480);
     room::camera_setup(room1,0,1,(GB_CamSetup){0,0,640,480,0},(GB_CamSetup){0,0,640,480,0},-1,(GB_CamTarget){0,0,0,0});
     room::add_instance(room1,objLinus,0,0,nullptr);
