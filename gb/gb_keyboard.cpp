1cb365c9d7189945544216f38be468b6c7047eee
elpoeprod <elpoepstudios@gmail.com>
2025-08-17 01:43:41 +0300

    more readable code

--- gb/gb_keyboard.cpp
+++ gb/gb_keyboard.cpp
@@ -4,9 +4,24 @@ namespace GameBreaker {
     std::map<std::string,int> mykey;
     std::map<std::string,int> mylastkey;
 
-    int keyboard::holding(int key) {return mykey[SDL_GetKeyName(key)]&&mylastkey[SDL_GetKeyName(key)];}
-    int keyboard::pressed(int key) {return mykey[SDL_GetKeyName(key)]&&!mylastkey[SDL_GetKeyName(key)];}
-    int keyboard::released(int key) {return !mykey[SDL_GetKeyName(key)]&&mylastkey[SDL_GetKeyName(key)];}
-    int keyboard::ord(char *key) {return (int)key[0];};
-    char *keyboard::chr(int ch) {return (char *)(ch+"\0");} //i think this is bad
+    int keyboard::holding(int key) {
+        return mykey[SDL_GetKeyName(key)]&&mylastkey[SDL_GetKeyName(key)];
+    }
+    
+    int keyboard::pressed(int key) {
+        return mykey[SDL_GetKeyName(key)]&&!mylastkey[SDL_GetKeyName(key)];
+    }
+    
+    int keyboard::released(int key) {
+        return !mykey[SDL_GetKeyName(key)]&&mylastkey[SDL_GetKeyName(key)];
+    }
+    
+    int keyboard::ord(char *key) {
+        return (int)key[0];
+    };
+    
+    char *keyboard::chr(int ch) {
+        return (char *)(ch+"\0");
+    } //i think this is bad
+
 }
