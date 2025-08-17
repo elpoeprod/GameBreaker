bd6be84dacc7befa75cfabdce7847b43fc58f64b
elpoeprod <elpoepstudios@gmail.com>
2025-08-17 23:13:35 +0300

    more documentation for window functions

--- gb/gb_window.cpp
+++ gb/gb_window.cpp
@@ -5,16 +5,23 @@
 
 namespace GameBreaker {
 
+	/**
+	* sets an icon for the window
+	**/
     void window::set_icon(gb_str ico) {
         SDL_Surface *temp=IMG_Load(ico.c_str());
         SDL_SetWindowIcon(gb_win->win,temp);
         SDL_FreeSurface(temp);
     }
 
+    /**
+    * sets a title for the window
+    **/
     void window::set_title(gb_str title) {
         SDL_SetWindowTitle(gb_win->win,title.c_str());
 
     }
+    
     /**
     * sets window width and height
     **/
@@ -24,6 +31,7 @@ namespace GameBreaker {
         gb_win->w = w;
         gb_win->h = h;
     }
+    
     /**
     * sets window position (x and y)
     **/
@@ -33,26 +41,40 @@ namespace GameBreaker {
         gb_win->x = x;
         gb_win->y = y;
     }
+
+    /**
+    * gets window coordinates on screen
+    **/
     SDL_Point get_pos() {
         int x=0,y=0;
         SDL_GetWindowPosition(gb_win->win, &x,&y);
         return (SDL_Point){x,y};
     }
+    
     /**
     * returns window width
     **/
-    int window::get_width() { return gb_win->w; }
+    int window::get_width() { 
+    	return gb_win->w; 
+    }
+
     /**
     * returns window height
     **/
-    int window::get_height() { return gb_win->h; }
+    int window::get_height() { 
+    	return gb_win->h; 
+    }
 
     /**
     * returns window size as a struct {w,h} that can be used somewhere
     **/
-    SDL_Point window::get_size() { return (SDL_Point) { gb_win->w, gb_win->h }; }
+    SDL_Point window::get_size() { 
+    	return (SDL_Point) { gb_win->w, gb_win->h }; 
+    }
 
-    SDL_Renderer* window::get_renderer() { return gb_win->ren; }
+    SDL_Renderer* window::get_renderer() { 
+    	return gb_win->ren; 
+    }
 
     void window::set_priority(int num) {
         SDL_SetThreadPriority((SDL_ThreadPriority)num); // 0 - low, 3 - high like on drugs
