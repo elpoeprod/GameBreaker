d7d45dfb30c3476820355fde320954e708cb9bdd
elpoeprod <elpoepstudios@gmail.com>
2025-08-27 20:26:05 +0300

    it doesn't work and lags so much so dont use it

--- gb/gb_msg.cpp
+++ gb/gb_msg.cpp
@@ -1,3 +1,4 @@
+#include <gtk/gtk.h>
 #include "../include/gamebreaker.hpp"
 
 namespace GameBreaker {
@@ -18,5 +19,25 @@ namespace GameBreaker {
             printf("ERROR: %s\nExit code: 0xC01001\n",msg.c_str());
             exit(0xc01001);
         }
+    }
+
+    int show::popover_menu(GBPOMItems items) {
+    	gtk_init(nullptr,nullptr);
+		int clicked=-1;
+		GtkWidget *button=gtk_button_new_with_label("none");
+		GtkWidget* popover = gtk_popover_new(button);
+	    GtkWidget* box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+
+	    for(long unsigned int _i=0;_i<items.size();_i++) {
+	    // Add items to the popover
+		    GtkWidget* item = gtk_button_new_with_label("Item 1");
+		    g_signal_connect(item, "clicked", G_CALLBACK(items[_i].func), NULL);
+		    gtk_box_pack_start(GTK_BOX(box), item, TRUE, TRUE, 0);
+	    }
+	    gtk_container_add(GTK_CONTAINER(popover), box);
+	    gtk_widget_show_all(popover);
+	    gtk_widget_show(popover);
+    	//gtk_main();
+    	return clicked;
     }
 }
