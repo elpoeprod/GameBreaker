3a82aadf5d1c8f9834c3e3832dfd10d1eb8e9b40
elpoeprod <elpoepstudios@gmail.com>
2025-12-13 20:36:19 +0300

    popover updates (wip)

--- gb/gb_msg.cpp
+++ gb/gb_msg.cpp
@@ -1,5 +1,16 @@
-#include <gtk/gtk.h>
+/*
+ * Messages, errors, etc functions
+ * 2025 elpoep
+*/
+//#include <gtk/gtk.h>
 #include "../include/gamebreaker.hpp"
+#include <SDL2/SDL_messagebox.h>
+
+typedef struct _GB_POM_T{
+	SDL_MessageBoxData *data;
+} _GB_POM_T;
+
+std::vector<_GB_POM_T *> _pom_db_;
 
 namespace GameBreaker {
 
@@ -13,26 +24,31 @@ namespace GameBreaker {
         SDL_ShowSimpleMessageBox(0, title.c_str(), msg.c_str(), gb_win->win);
     }
 
-    void show::error(gb_str msg, int abort) {
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "FATAL ERROR", msg.c_str(), gb_win->win);
-        if(abort) {
+	void show::error(gb_str msg, int abort) {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "FATAL ERROR", msg.c_str(), gb_win->win);
+		if(abort) {
             printf("ERROR: %s\nExit code: 0xC01001\n",msg.c_str());
-            exit(0xc01001);
-        }
-    }
-
-    int show::popover_menu(gb_str file) {
-    	gtk_init(nullptr,nullptr);
-		int clicked=-1;
-		GtkBuilder *builder=gtk_builder_new();
-		gtk_builder_add_from_file(builder,file.c_str(),NULL);
-		
-		GtkWidget *mymenu=GTK_WIDGET(gtk_builder_get_object(builder,"popover"));
-		g_signal_connect(mymenu,"destroy",G_CALLBACK(gtk_main_quit),NULL);
-		gtk_builder_connect_signals(builder, NULL);
-
-		gtk_widget_show_all(mymenu);
-		gtk_main();
-    	return clicked;
-    }
+			exit(0xc01001);
+		}
+	}
+	int popover::create() {
+		SDL_MessageBoxData *mydata=new SDL_MessageBoxData;//{.flags=SDL_MESSAGEBOX_BUTTONS_LEFT_TO_RIGHT,.window=gb_win->win,.title="PopOverMenu"};
+		mydata->title="PopOverMenu";
+		mydata->window=gb_win->win;
+		mydata->flags=SDL_MESSAGEBOX_BUTTONS_LEFT_TO_RIGHT;
+		_GB_POM_T *mypom=new _GB_POM_T;
+		mypom->data=mydata;
+		_pom_db_.push_back(
+			mypom
+		);
+		return _pom_db_.size()-1;
+	}
+	/* TODO
+	int popover::add_entry(int menu_id,gb_str str) {
+		const SDL_MessageBoxButtonData mybutdata={.buttonid=_pom_db_[menu_id]->data->numbuttons+1,.text=str.c_str()};
+		_pom_db_[menu_id]->data->numbuttons++;
+		_pom_db_[menu_id]->data->buttons[_pom_db_[menu_id]->data->numbuttons-1]=mybutdata;
+		return mybutdata->buttonid;
+	}
+	*/
 }
