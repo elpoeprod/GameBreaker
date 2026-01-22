347235635171126feb3d7ac7b55df4db2bd8a3d5
elpoeprod <elpoepstudios@gmail.com>
2026-01-22 18:10:13 +0300

    updated example

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,44 +1,47 @@
 #include "../include/gamebreaker.hpp"
 
-gb::object myobj;
-gb::object myobj2;
-gb::system sys;
-gb::room *room0;
-gb::sprite *spr0;
-gb::sound *snd0;
+using namespace gb;
 
-void myobj_event_draw(gb::object *self) {
-	gb::draw::color({255,0,255,255});
-	gb::draw::rect({30,30,60,60},0);
-	if(gb::mouse::pressed(gb::mb::any)) {
+object myobj;
+object myobj2;
+gb::system sys;
+room *room0;
+sprite *spr0;
+sound *snd0;
+
+void myobj_event_draw(object *self) {
+	draw::color({255,0,255,255});
+	draw::rect({30,30,60,60},0);
+	if(mouse::pressed(mb::any)) {
 		self->depth=-self->depth;
 		myobj2.depth=-myobj2.depth;
 	}
-	//gb::debug_message("I'm on depth "+std::to_string(self->depth)+"! 1");
+	self->x+=(keyboard::holding(vk_right)-keyboard::holding(vk_left))*3;
+	//debug_message("I'm on depth "+std::to_string(self->depth)+"! 1");
 	return;
 }
 
-void myobj2_event_draw(gb::object *self) {
+void myobj2_event_draw(object *self) {
 	//spr0->draw({(float)self->x, (float)self->y+10}, 0);
-	gb::draw::color({255,255,255,255});
-	gb::draw::rect({0,0,60,60},0);
+	draw::color({255,255,255,255});
+	draw::rect({0,0,60,60},0);
 	spr0->draw({50,50},1);
-	//gb::debug_message("I'm on depth "+std::to_string(self->depth)+"! 2");
+	//debug_message("I'm on depth "+std::to_string(self->depth)+"! 2");
 	return;
 }
 
 int main(void) {
-	// myobj=	new gb::object;
-	// myobj2=	new gb::object;
-	room0=	new gb::room;
-	spr0=	new gb::sprite;
-	snd0=	new gb::sound;
+	// myobj=	new object;
+	// myobj2=	new object;
+	room0=	new room;
+	spr0=	new sprite;
+	snd0=	new sound;
 	
-	//gb::debug_mode=1;
+	//debug_mode=1;
 	
 	sys.init();
 
-	snd0->add("d.mp3",gb::SOUND_TYPE::stream);
+	snd0->add("d.mp3",SOUND_TYPE::stream);
 	snd0->play();
 	
 	spr0->add("c.png",1,{0,0});
@@ -47,8 +50,8 @@ int main(void) {
 	room0->add({640,480});
 	
 	room0->current();
-	room0->add_instance(&myobj,{0,0});
-	room0->add_instance(&myobj2,{10,10});
+	instance::create(&myobj,{0,0});
+	instance::create(&myobj2,{10,10});
 	
 	myobj.depth=10;
 	myobj2.depth=-10;
