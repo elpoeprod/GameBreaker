d8284499a2a758fd9f4b39907deffd962a4d21d7
elpoeprod <elpoepstudios@gmail.com>
2026-01-23 00:15:13 +0300

    updated example

--- examples/test.cpp
+++ examples/test.cpp
@@ -5,11 +5,12 @@ using namespace gb;
 object myobj;
 object myobj2;
 gb::system sys;
-room *room0;
-sprite *spr0;
-sound *snd0;
+room room0;
+sprite spr0;
+sound snd0;
 
 void myobj_event_draw(object *self) {
+	//d3d::start();
 	draw::color({255,0,255,255});
 	draw::rect({30,30,60,60},0);
 	if(mouse::pressed(mb::any)) {
@@ -17,39 +18,32 @@ void myobj_event_draw(object *self) {
 		myobj2.depth=-myobj2.depth;
 	}
 	self->x+=(keyboard::holding(vk_right)-keyboard::holding(vk_left))*3;
-	//debug_message("I'm on depth "+std::to_string(self->depth)+"! 1");
+	//d3d::end();
 	return;
 }
 
 void myobj2_event_draw(object *self) {
-	//spr0->draw({(float)self->x, (float)self->y+10}, 0);
 	draw::color({255,255,255,255});
 	draw::rect({0,0,60,60},0);
-	spr0->draw({50,50},1);
-	//debug_message("I'm on depth "+std::to_string(self->depth)+"! 2");
+	spr0.draw({50,50},1);
 	return;
 }
 
 int main(void) {
-	// myobj=	new object;
-	// myobj2=	new object;
-	room0=	new room;
-	spr0=	new sprite;
-	snd0=	new sound;
 	
 	//debug_mode=1;
 	
 	sys.init();
 
-	snd0->add("d.mp3",SOUND_TYPE::stream);
-	snd0->play();
+	snd0.add("d.mp3",SOUND_TYPE::stream);
+	snd0.play();
 	
-	spr0->add("c.png",1,{0,0});
-	myobj.add(spr0);
-	myobj2.add(spr0);
-	room0->add({640,480});
+	spr0.add("c.png",1,{0,0});
+	myobj.add(&spr0);
+	myobj2.add(&spr0);
+	room0.add({640,480});
 	
-	room0->current();
+	room0.current();
 	instance::create(&myobj,{0,0});
 	instance::create(&myobj2,{10,10});
 	
