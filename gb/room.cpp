fce8617c67f127d0035bbf482192999c153cdf2d
elpoeprod <elpoepstudios@gmail.com>
2026-01-22 17:33:39 +0300

    rename instance functions to `instance::*` and add new

--- gb/room.cpp
+++ gb/room.cpp
@@ -1,5 +1,10 @@
 #include "../include/gamebreaker.hpp"
 
+
+// To not get GB confused, room's instance ids are starting with object_id_adder value.
+// GB can get confused because it can delete every object instance, and not only one object instance.
+int object_id_adder=1000000;
+
 namespace GameBreaker {
 	void room::add(GBSize size) {
 		this->size=size;
@@ -20,8 +25,13 @@ namespace GameBreaker {
 		return;
 	}
 
-	object *room::get_instance(int id) {
-		return this->robjects[id];
+	object *instance::get(int id) {
+		if(id==GB_INSTANCE_ANY) {debug_message("instance::get() doesn't accept GB_INSTANCE_ANY."); return nullptr;}
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		if(id>object_id_adder) 
+			return myobj[id-object_id_adder-1];
+		else
+			return myobj[id];
 	}
 
 	std::vector<object *> *room::__get_room_objects() {
@@ -33,21 +43,92 @@ namespace GameBreaker {
 		return;
 	}
 	
-	luint room::instance_count(int id) {
-		if(id==GB_INSTANCE_ANY) return this->robjects.size();
+	luint instance::count(int id) {
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		if(id==GB_INSTANCE_ANY) return myobj.size();
 		else {
 			int myn=0;
-			for (luint i = 0; i < this->robjects.size(); i++) {
-				if(i==(luint)id) myn++;
+			for (luint i = 0; i < myobj.size(); i++) {
+				if(i==(luint)id-object_id_adder-1) myn++;
 			}
 			return myn;
 		}
 		return 0;
 	}
 
-	void room::add_instance(object *obj, GBPoint pos) {
+	luint instance::create(object *obj, GBPoint pos) {
 		obj->x=pos.x;
 		obj->y=pos.y;
-		this->robjects.push_back(obj);
+		_gbsys_->current_room()->__add(obj);
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		return myobj.size()+object_id_adder;
+	}
+
+	void instance::destroy(int id) {
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		if(id==GB_INSTANCE_ANY) 
+			myobj.erase(myobj.begin(),myobj.end());
+		else {
+			if(id>object_id_adder) {
+				for(luint i=0;i<myobj.size();i++) {
+					if(i==(luint)id-object_id_adder-1) myobj.erase(myobj.begin()+i);
+				}
+			} else {
+				for(luint i=0;i<myobj.size();i++) {
+					if(myobj[i]->id==id) myobj.erase(myobj.begin()+i);
+				}
+			}
+		}
+		_gbsys_->current_room()->__set_room_objects(myobj);
+		return;
+	}
+
+	int instance::nearest(GBPoint pos, int obj_id) {
+		if(obj_id>object_id_adder) return -1;
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		GB_NumberBag mybag={};
+		for(luint i=0;i<myobj.size();i++) {
+			if(myobj[i]->id==obj_id||obj_id==GB_INSTANCE_ANY) {
+				mybag.push_back(math::pdistance(pos,{myobj[i]->x,myobj[i]->y}));
+			}
+		}
+		return math::min(mybag);
+	}
+
+	int instance::furthest(GBPoint pos, int obj_id) {
+		if(obj_id>object_id_adder) return -1;
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		GB_NumberBag mybag={};
+		for(luint i=0;i<myobj.size();i++) {
+			if(myobj[i]->id==obj_id||obj_id==GB_INSTANCE_ANY) {
+				mybag.push_back(math::pdistance(pos,{myobj[i]->x,myobj[i]->y}));
+			}
+		}
+		return math::max(mybag);
+	}
+
+	void instance::pos_destroy(GBPoint pos) {
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		for(luint i=0;i<myobj.size();i++) {
+			if(myobj[i]->x==pos.x&&myobj[i]->y==pos.y) myobj.erase(myobj.begin()+i);
+		}
+		_gbsys_->current_room()->__set_room_objects(myobj);
+		return;
+	}
+
+	int instance::exists(int id) {
+		auto myobj=*_gbsys_->current_room()->__get_room_objects();
+		if(id==GB_INSTANCE_ANY) return myobj.size()>0;
+		if(id>object_id_adder) {
+			for(luint i=0;i<myobj.size();i++) {
+				if(i==(luint)id-object_id_adder-1) return 1;
+			}
+		} else {
+			for(luint i=0;i<myobj.size();i++) {
+				if(myobj[i]->id==id) return 1;
+			}
+		}
+
+		return 0;
 	}
 }
